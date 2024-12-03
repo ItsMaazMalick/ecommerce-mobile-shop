29cf0580e859c3bd9e1c21a4dc9fbd0bbd59299e
ItsMaazMalick <maaz93707@gmail.com>
2024-12-03 21:46:38 +0500

    check

--- src/app/(frontend)/dashboard/repairing-services/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAllProductWithCategoryName } from "@/actions/product";
-import { RepairingServices } from "./repairing-services";
-import { getAllRepairingProducts } from "@/actions/repairing";
-
-export default async function RepairingServicesPage() {
-  const products = await getAllRepairingProducts();
-  return <RepairingServices products={products} />;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen w-full`}
       >
-        {/* {children} */}
-        HELLO
+        {children}
       </body>
     </html>
   );
